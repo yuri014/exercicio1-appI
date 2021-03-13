84d13a1faf5f2538d65a766729f4baea864ca9e0
yuri014 <yuriflores2012@gmail.com>
2021-03-13 16:59:36 -0300

    main incompleta

---
+++ src/App.java
@@ -0,0 +1,28 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        AverageCalculator average = new AverageCalculator();
+        ProductCalculator products = new ProductCalculator();
+        SquareAreaCalculator square = new SquareAreaCalculator();
+        PriceCalculator price = new PriceCalculator();
+        DiscountCalculator priceAndDiscount = new DiscountCalculator();
+        InvertValues values = new InvertValues();
+
+        average.getGrades();
+        average.calculate();
+
+        products.getNumbers();
+        products.calculate();
+
+        square.getSquareSide();
+        square.calculate();
+
+        price.getPrice();
+        price.calculate();
+
+        priceAndDiscount.getPriceAndDiscount();
+        priceAndDiscount.calculate();
+
+        values.getValues();
+        values.invert();
+    }
+}
